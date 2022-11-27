dcc4eef8418d391c2b1ae5a2cca1d524ba9cb8bb
Alvin Mithamo <alvinmithamo100@gmail.com>
2022-11-28 00:25:24 +0300

    Speeddetector

---
+++ speeddetector.js
@@ -0,0 +1,15 @@
+function detector(inputspeed) {
+
+    let points;
+
+    if(inputspeed<70)
+    console.log(“Ok”)
+    else
+        {
+    console.log("Points:"+((inputspeed-70)/5));
+
+    if[(inputspeed-70)/5] > 12;
+
+    console.log(“License suspended.”);
+}
+}
