817cc17f4783dd6870158b920d36eb264fd7e74f
Alvin Mithamo <117805605+alvinsleek@users.noreply.github.com>
2022-12-09 10:46:27 +0300

    Update studentgradecalculator.js

--- studentgradecalculator.js
+++ studentgradecalculator.js
@@ -1,25 +1,24 @@
-//Here is my student grade calculator code
-
 // we declare the function with a parameter(grades)
 
 function grades(input){ 
 //input entered must be an integer between 0 - 100
-    if (input < 0 || input > 100){
+    const input = parseInt(prompt('enter a student mark:));
+    if (isNan(input) ||input < 0 || input > 100){
         console.log("Enter a valid input")
 
-        let grade = ''
     //if...else loop is used to distinguish the grades through use of comparison operators
-    }else{
-        if (marks > 79) {
+        let grade;
+        if (marks >= 80) {
             grade = 'A';    
-        } else if (marks > 60) {
+        } else if (marks >= 60) {
             grade = 'B';   
-        } else if(marks > 49) {
+        } else if(marks >= 50) {
             grade = 'C';
-        } else if(marks > 40) {
+        } else if(marks >= 40) {
             grade = 'D';
         }else{
             grade = 'E';
         }
+    return grade;
     }
 }
